0fccd74272556c3fc503cf65c3777ae28ba6365e
Vincent Tavernier <vincent.tavernier@ensimag.grenoble-inp.fr>
2015-08-12 21:26:11 +0200

    Removed /inte mounting point (replaced by Nginx/Passenger)

--- config/routes.rb
+++ config/routes.rb
@@ -2,34 +2,24 @@ Rails.application.routes.draw do
   # Application homepage
   root 'home#index'
 
-  draw_routes = Proc.new do
-    get '/teams' => "home#teams", as: :teams
-    get '/stats' => "home#stats", as: :stats
-    get '/rewards' => "home#rewards", as: :rewards
+  get '/teams' => "home#teams", as: :teams
+  get '/stats' => "home#stats", as: :stats
+  get '/rewards' => "home#rewards", as: :rewards
 
-    # Event streaming
-    get '/events' => "home#events", as: :events
+  # Event streaming
+  get '/events' => "home#events", as: :events
 
-    # Admin routes, require authentication
-    authenticate :user do
-      get '/admin' => "home#admin",    as: :admin
-      post '/points' => "home#points", as: :points
-    end
-
-    # Login pages
-    devise_for :users, controllers: { sessions: "users/sessions" }, skip: [ :sessions ]
-    devise_scope :user do
-      get    "login"   => "users/sessions#new",         as: :new_user_session
-      post   "login"   => "users/sessions#create",      as: :user_session
-      delete "signout" => "users/sessions#destroy",     as: :destroy_user_session
-    end
+  # Admin routes, require authentication
+  authenticate :user do
+    get '/admin' => "home#admin",    as: :admin
+    post '/points' => "home#points", as: :points
   end
 
-  if Rails::configuration.application_prefix
-    scope Rails::configuration.application_prefix do
-      draw_routes.call
-    end
-  else
-    draw_routes.call
+  # Login pages
+  devise_for :users, controllers: { sessions: "users/sessions" }, skip: [ :sessions ]
+  devise_scope :user do
+    get    "login"   => "users/sessions#new",         as: :new_user_session
+    post   "login"   => "users/sessions#create",      as: :user_session
+    delete "signout" => "users/sessions#destroy",     as: :destroy_user_session
   end
 end
